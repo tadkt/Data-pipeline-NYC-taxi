e779847e1a96939d9e80c43ed71d341c78601c5a
Tadkt <thaidoankien@gmail.com>
2024-10-27 10:20:10 +0700

    Commit 27/10

--- batch_processing/datalake_to_dw.py
+++ batch_processing/datalake_to_dw.py
@@ -14,4 +14,26 @@ sys.path.append(utils_path)
 from helpers import load_cfg
 from minio_utils import MinIOClient
 
-logging.basicConfig()
+logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(funcname)s:%(levelname)s:%(message)s')
+warnings.filterwarnings('ignore')
+
+#################################
+# Parameters & Arguments
+#################################
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_HOST = os.getenv('POSTGRES_HOST')
+DB_STAGING_TABLE = os.getenv('DB_STAGING_TABLE')
+
+CFG_FILE = "./config/datalake.yaml"
+cfg = load_cfg(CFG_FILE)
+datalake_cfg = cfg['datalake'] # Transform into a dictionary-like variable
+
+MINIO_ENDPOINT = datalake_cfg['endpoint']
+MINIO_ACCESSKEY = datalake_cfg['access_key']
+MINIO_SECRETKEY = datalake_cfg['secret_key']
+BUCKET_NAME = datalake_cfg['bucket_name_2']
+
+CFG_FILE_SPARK = "./config/spark.yaml"
+
