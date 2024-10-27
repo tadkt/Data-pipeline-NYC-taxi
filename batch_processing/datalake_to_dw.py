5826ef0625e63aea700d983265567991b62fb3d0
Tadkt <thaidoankien@gmail.com>
2024-10-27 10:19:32 +0700

    Commit 27/10

--- batch_processing/datalake_to_dw.py
+++ batch_processing/datalake_to_dw.py
@@ -9,8 +9,9 @@ dotenv.load_dotenv('.env')
 
 from pyspark import SparkConf, SparkContext
 
-utils_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'utils'))
+utils_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'utils')) # Going back to utils directory
 sys.path.append(utils_path)
-print(utils_path)
-# from helpers import load_cfg
-# from minio_utils import MinIOClient
+from helpers import load_cfg
+from minio_utils import MinIOClient
+
+logging.basicConfig()
